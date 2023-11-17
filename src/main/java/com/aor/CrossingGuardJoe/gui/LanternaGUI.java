f57c6d858854abbf052b973d44001044401e602d
Huang <brunohuang2012gmail.com>
2023-11-17 12:38:14 +0000

    Added LanternaGUI and Game classes

---
+++ src/main/java/com/aor/CrossingGuardJoe/gui/LanternaGUI.java
@@ -0,0 +1,37 @@
+package com.aor.CrossingGuardJoe.gui;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class LanternaGUI {
+    public TextGraphics tg;
+    public LanternaGUI() throws IOException {
+        TerminalSize terminalSize = new TerminalSize(1000, 500);
+        Terminal terminal = new DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize)
+                .setTerminalEmulatorFontConfiguration(
+                        SwingTerminalFontConfiguration.newInstance(new Font("Monospaced", Font.PLAIN, 1)))
+                .createTerminal();
+
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+
+        this.tg = screen.newTextGraphics();
+        screen.startScreen();
+
+        tg.setBackgroundColor(TextColor.Factory.fromString("#7f7976"));
+        tg.fill(' ');
+
+        screen.refresh();
+    }
+}
