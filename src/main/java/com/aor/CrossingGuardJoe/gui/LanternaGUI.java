407e3eb2e791209df250ec136f879b25ef7d889f
Huang <brunohuang2012gmail.com>
2023-11-18 23:09:22 +0000

    Organized code related to kid movement

--- src/main/java/com/aor/CrossingGuardJoe/gui/LanternaGUI.java
+++ src/main/java/com/aor/CrossingGuardJoe/gui/LanternaGUI.java
@@ -39,7 +39,7 @@ public class LanternaGUI {
 
         this.kid = new Kid(100, 100);
         KidView kidView = new KidView(tg, kid);
-        this.kidController = new KidController(kid, kidView);
+        this.kidController = new KidController(kid);
 
 
         tg.setBackgroundColor(TextColor.Factory.fromString("#7f7976"));
@@ -56,6 +56,7 @@ public class LanternaGUI {
                 }
 
                 if (keyStroke.getKeyType() == KeyType.ArrowLeft) {
+                    kidController.moveLeft();
                     screen.clear();
                     kidView.draw();
                     screen.refresh();
