1065795ed277c4808913e7f7f0235ee1784079a8
Huang <brunohuang2012gmail.com>
2023-11-23 14:40:10 +0000

    implemented GameViewer and updated GameState

--- src/main/java/com/aor/CrossingGuardJoe/states/GameState.java
+++ src/main/java/com/aor/CrossingGuardJoe/states/GameState.java
@@ -1,21 +1,21 @@
 package com.aor.CrossingGuardJoe.states;
 
 import com.aor.CrossingGuardJoe.controller.Controller;
+import com.aor.CrossingGuardJoe.controller.game.RoadController;
 import com.aor.CrossingGuardJoe.model.game.Road;
 import com.aor.CrossingGuardJoe.viewer.Viewer;
+import com.aor.CrossingGuardJoe.viewer.game.GameViewer;
 
 public class GameState extends State<Road>{
     public GameState(Road road) { super(road);}
 
     @Override
     public Controller getController() {
-        return null;
-        //return new GameViewer(getModel());
+        return new RoadController(getModel());
     }
 
     @Override
     public Viewer getViewer() {
-        return null;
-        //return new RoadController(getModel());
+        return new GameViewer(getModel());
     }
 }
---
+++ src/main/java/com/aor/CrossingGuardJoe/viewer/game/GameViewer.java
@@ -0,0 +1,10 @@
+package com.aor.CrossingGuardJoe.viewer.game;
+
+import com.aor.CrossingGuardJoe.model.game.Road;
+import com.aor.CrossingGuardJoe.viewer.Viewer;
+
+public class GameViewer extends Viewer<Road> {
+    public GameViewer(Road model) {
+        super(model);
+    }
+}
