c5c7435c72afa39a28eca3a385eae4e9da1833ef
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-05 19:44:38 +0000

    statsMenu State

--- src/main/java/com/CrossingGuardJoe/states/StatsMenuStates.java
+++ src/main/java/com/CrossingGuardJoe/states/StatsMenuStates.java
@@ -1,4 +1,23 @@
 package com.CrossingGuardJoe.states;
 
-public class StatsMenuStates {
+import com.CrossingGuardJoe.controller.Controller;
+import com.CrossingGuardJoe.controller.menu.StatsMenuController;
+import com.CrossingGuardJoe.model.menu.StatsMenu;
+import com.CrossingGuardJoe.viewer.Viewer;
+import com.CrossingGuardJoe.viewer.menu.StatsMenuViewer;
+
+public class StatsMenuStates extends State<StatsMenu> {
+    public StatsMenuStates(StatsMenu model) {
+        super(model);
+    }
+
+    @Override
+    public Controller<StatsMenu> getController() {
+        return new StatsMenuController(getModel());
+    }
+
+    @Override
+    public Viewer<StatsMenu> getViewer() {
+        return new StatsMenuViewer(getModel());
+    }
 }
