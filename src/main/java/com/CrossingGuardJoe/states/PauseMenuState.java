e8a43f5dcb01fffae12be72ce35bed3139ddf173
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-05 19:19:17 +0000

    implemented pauseMenu state

--- src/main/java/com/CrossingGuardJoe/states/PauseMenuState.java
+++ src/main/java/com/CrossingGuardJoe/states/PauseMenuState.java
@@ -1,8 +1,10 @@
 package com.CrossingGuardJoe.states;
 
 import com.CrossingGuardJoe.controller.Controller;
+import com.CrossingGuardJoe.controller.menu.PauseMenuController;
 import com.CrossingGuardJoe.model.menu.PauseMenu;
 import com.CrossingGuardJoe.viewer.Viewer;
+import com.CrossingGuardJoe.viewer.menu.PauseMenuViewer;
 
 public class PauseMenuState extends State<PauseMenu> {
     public PauseMenuState(PauseMenu model) {
@@ -11,11 +13,11 @@ public class PauseMenuState extends State<PauseMenu> {
 
     @Override
     public Controller<PauseMenu> getController() {
-        return new ;
+        return new PauseMenuController(getModel());
     }
 
     @Override
     public Viewer<PauseMenu> getViewer() {
-        return null;
+        return new PauseMenuViewer(getModel());
     }
 }
