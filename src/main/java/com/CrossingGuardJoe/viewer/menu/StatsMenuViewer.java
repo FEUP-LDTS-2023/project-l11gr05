bda7be8f4c481f2765bbb104302fba9756368e6b
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-05 19:41:33 +0000

    statsmenu viewer

--- src/main/java/com/CrossingGuardJoe/viewer/menu/StatsMenuViewer.java
+++ src/main/java/com/CrossingGuardJoe/viewer/menu/StatsMenuViewer.java
@@ -1,4 +1,33 @@
 package com.CrossingGuardJoe.viewer.menu;
 
-public class StatsMenuViewer {
+import com.CrossingGuardJoe.gui.GUI;
+import com.CrossingGuardJoe.model.Position;
+import com.CrossingGuardJoe.model.menu.StatsMenu;
+import com.CrossingGuardJoe.viewer.Viewer;
+
+public class StatsMenuViewer extends Viewer<StatsMenu> implements MenuInformationDrawer {
+    public StatsMenuViewer(StatsMenu model) {
+        super(model);
+    }
+
+    @Override
+    protected void drawElements(GUI gui) {
+        drawTitle(gui);
+        drawInformation(gui);
+    }
+
+    @Override
+    public void drawTitle(GUI gui) {
+        gui.drawText(new Position(208, 102), "Game stats", "#000000");
+        gui.drawText(new Position(208, 101), "Game stats", "#000000");
+        gui.drawText(new Position(207, 100), "Game stats", "#FFFFFF");
+    }
+
+    @Override
+    public void drawInformation(GUI gui) {
+        gui.drawText(new Position(230, 210), "score", "#FFFFFF");
+        gui.drawText(new Position(230, 250), "level", "#FFFFFF");
+        gui.drawText(new Position(200, 300), "highest score", "#FFFFFF");
+        gui.drawText(new Position(200, 340), "highest level", "#FFFFFF");
+    }
 }
