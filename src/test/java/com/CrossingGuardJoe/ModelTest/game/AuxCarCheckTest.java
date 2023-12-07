1ab55b9e81193d05528cf4fda0ceb37530fe8538
Huang <brunohuang2012gmail.com>
2023-12-07 17:00:02 +0000

    carcontroller test 100% coverage

--- src/test/java/com/CrossingGuardJoe/ModelTest/game/AuxCarCheckTest.java
+++ src/test/java/com/CrossingGuardJoe/ModelTest/game/AuxCarCheckTest.java
@@ -14,21 +14,17 @@ public class AuxCarCheckTest {
 
     @Test
     public void testIsAnyCarOverlapping() {
-        int newY = 50;
+        int newY = 70;
         int minDistance = 20;
 
-        // Create a list of cars with one car at position 60
         List<Car> cars = new ArrayList<>();
         cars.add(new Car(0, 60));
 
-        // Test when there is an overlapping car
         assertTrue(AuxCarCheck.isAnyCarOverlapping(newY, cars, minDistance));
 
-        // Test when there is no overlapping car
         newY = 80;
         assertFalse(AuxCarCheck.isAnyCarOverlapping(newY, cars, minDistance));
 
-        // Test when the list of cars is empty
         cars.clear();
         assertFalse(AuxCarCheck.isAnyCarOverlapping(newY, cars, minDistance));
     }
