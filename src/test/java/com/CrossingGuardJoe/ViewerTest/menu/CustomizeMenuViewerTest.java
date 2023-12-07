3c012418cc6e35f25e276c87e1ada7a1c776679c
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-07 22:30:03 +0000

    error with customize Menu viewer tests

--- src/test/java/com/CrossingGuardJoe/ViewerTest/menu/CustomizeMenuViewerTest.java
+++ src/test/java/com/CrossingGuardJoe/ViewerTest/menu/CustomizeMenuViewerTest.java
@@ -6,12 +6,17 @@ import com.CrossingGuardJoe.model.menu.CustomizeMenu;
 import com.CrossingGuardJoe.model.Position;
 import com.CrossingGuardJoe.model.menu.Option;
 import com.CrossingGuardJoe.viewer.Color;
+import com.CrossingGuardJoe.viewer.images.Font.FontImageFactory;
+import com.CrossingGuardJoe.viewer.images.defined.CarImage;
+import com.CrossingGuardJoe.viewer.images.defined.JoeImages;
+import com.CrossingGuardJoe.viewer.images.defined.KidImages;
 import com.CrossingGuardJoe.viewer.images.defined.ToolImages;
 import com.CrossingGuardJoe.viewer.images.generator.Shape;
 import com.CrossingGuardJoe.viewer.menu.CustomizeMenuViewer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +27,7 @@ public class CustomizeMenuViewerTest {
     GUI guiMock;
     CustomizeMenuViewer customizeMenuViewer;
     CustomizeMenu customizeMenuMock;
+    Option optionMock;
 
     String[] ARROW_RIGHT = {
             "$$",
@@ -79,6 +85,7 @@ public class CustomizeMenuViewerTest {
         guiMock = mock(GUI.class);
         customizeMenuMock = mock(CustomizeMenu.class);
         customizeMenuViewer = new CustomizeMenuViewer(customizeMenuMock);
+        optionMock = mock(Option.class);
     }
 
     @Test
@@ -96,30 +103,28 @@ public class CustomizeMenuViewerTest {
     }
 
     @Test
-    public void drawElements() {
-        Option optionMock = mock(Option.class);
+    public void testDrawElements() {
+        when(optionMock.name()).thenReturn("");
+        when(optionMock.position()).thenReturn(new Position(10, 20));
+        when(optionMock.image()).thenReturn(JoeImages.JOE_PASS);
+        when(customizeMenuMock.getMenuLevels().get(0)).thenReturn(Arrays.asList(optionMock));
+        when(customizeMenuMock.isSelectedJoeCustomize()).thenReturn(true);
 
-        List<List<Option>> menuLevels = Arrays.asList(Arrays.asList(optionMock));
-        when(customizeMenuMock.getMenuLevels()).thenReturn(menuLevels);
-        when(customizeMenuMock.isSelectedElement(anyInt())).thenReturn(true);
-        when(customizeMenuMock.isSelectedOption(anyInt(), anyInt())).thenReturn(true);
-        when(optionMock.position()).thenReturn(new Position(100, 100));
-        when(optionMock.image()).thenReturn(null);
-        when(optionMock.name()).thenReturn("Option Name");
 
-        CustomizeMenuViewer customizeMenuViewer = new CustomizeMenuViewer(customizeMenuMock);
+        int SELECTION_BOX_WIDTH = 235;
+        int SELECTION_BOX_HEIGHT = 300;
 
         customizeMenuViewer.drawElements(guiMock);
 
-        verify(guiMock).drawImage(new Position(40, 70), Shape.RectangleFilledGenerator(235, 300, ' ', 2, 'G'));
-        verify(guiMock).drawImage(new Position(100, 100), null);
-        verify(guiMock).drawText(new Position(99, 99), "Option Name", "#FFFFFF");
-        verify(guiMock).drawImage(new Position(70, 100), ARROW_RIGHT);
 
-        verify(customizeMenuMock, times(1)).isSelectedElement(anyInt());
-        verify(optionMock, times(1)).position();
-        verify(optionMock, times(1)).image();
-        verify(optionMock, times(1)).name();
+        verify(guiMock).drawImage(new Position(40, 70), Shape.RectangleFilledGenerator(SELECTION_BOX_WIDTH, SELECTION_BOX_HEIGHT, ' ', 2, 'G'));
+
+
+        verify(guiMock).drawImage(new Position(40, 70), Shape.RectangleFilledGenerator(SELECTION_BOX_WIDTH, SELECTION_BOX_HEIGHT, 'K', 2, '$'));
+        verify(guiMock).drawImage(new Position(195, 70), Shape.RectangleFilledGenerator(SELECTION_BOX_WIDTH, SELECTION_BOX_HEIGHT, 'K', 2, '$'));
+        verify(guiMock).drawImage(new Position(350, 70), Shape.RectangleFilledGenerator(SELECTION_BOX_WIDTH, SELECTION_BOX_HEIGHT, 'K', 2, '$'));
+
     }
+
 
 }
