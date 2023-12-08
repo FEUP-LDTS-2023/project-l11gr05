9ab38d2f5be79907edd04112ddd474ef9951c574
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-08 19:03:31 +0000

    Customize Menu tests 1/3

--- src/test/java/com/CrossingGuardJoe/ViewerTest/menu/CustomizeMenuViewerTest.java
+++ src/test/java/com/CrossingGuardJoe/ViewerTest/menu/CustomizeMenuViewerTest.java
@@ -7,10 +7,7 @@ import com.CrossingGuardJoe.model.Position;
 import com.CrossingGuardJoe.model.menu.Option;
 import com.CrossingGuardJoe.viewer.Color;
 import com.CrossingGuardJoe.viewer.images.Font.FontImageFactory;
-import com.CrossingGuardJoe.viewer.images.defined.CarImage;
-import com.CrossingGuardJoe.viewer.images.defined.JoeImages;
-import com.CrossingGuardJoe.viewer.images.defined.KidImages;
-import com.CrossingGuardJoe.viewer.images.defined.ToolImages;
+import com.CrossingGuardJoe.viewer.images.defined.*;
 import com.CrossingGuardJoe.viewer.images.generator.Shape;
 import com.CrossingGuardJoe.viewer.menu.CustomizeMenuViewer;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,7 +24,9 @@ public class CustomizeMenuViewerTest {
     GUI guiMock;
     CustomizeMenuViewer customizeMenuViewer;
     CustomizeMenu customizeMenuMock;
-
+    ColorPaletteMenu colorPaletteMenuMock;
+    int SELECTION_BOX_WIDTH = 235;
+    int SELECTION_BOX_HEIGHT = 300;
 
     String[] ARROW_RIGHT = {
             "$$",
@@ -85,6 +84,37 @@ public class CustomizeMenuViewerTest {
         guiMock = mock(GUI.class);
         customizeMenuMock = mock(CustomizeMenu.class);
         customizeMenuViewer = new CustomizeMenuViewer(customizeMenuMock);
+        colorPaletteMenuMock = mock(ColorPaletteMenu.class);
+        when(customizeMenuMock.getColorPaletteMenu()).thenReturn(colorPaletteMenuMock);
+
+        int JOE_X = 80;
+        int OPTIONS_Y = 230;
+        List<Option> Joe = Arrays.asList(
+                new Option("", new Position(JOE_X - 2, 95), JoeImages.JOE_STAND),
+                new Option("Cap", new Position(JOE_X, OPTIONS_Y), new FontImageFactory().getImageRepresentation("Cap")),
+                new Option("Clothes", new Position(JOE_X, OPTIONS_Y + 30), new FontImageFactory().getImageRepresentation("Clothes")),
+                new Option("Vest", new Position(JOE_X, OPTIONS_Y + 60), new FontImageFactory().getImageRepresentation("Vest")),
+                new Option("Shoes", new Position(JOE_X, OPTIONS_Y + 90), new FontImageFactory().getImageRepresentation("Shoes"))
+        );
+
+        int KIDS_X = 235;
+        List<Option> Kids = Arrays.asList(
+                new Option("", new Position(KIDS_X + 5, 115), KidImages.KID_STAND),
+                new Option("Shirt", new Position(KIDS_X, OPTIONS_Y), new FontImageFactory().getImageRepresentation("Shirt")),
+                new Option("Pants", new Position(KIDS_X, OPTIONS_Y + 30), new FontImageFactory().getImageRepresentation("Pants")),
+                new Option("BackPack", new Position(KIDS_X, OPTIONS_Y + 60), new FontImageFactory().getImageRepresentation("Backpack")),
+                new Option("Shoes", new Position(KIDS_X, OPTIONS_Y + 90), new FontImageFactory().getImageRepresentation("Shoes"))
+        );
+
+        int CARS_X = 403;
+        List<Option> Cars = Arrays.asList(
+                new Option("", new Position(CARS_X - 27, 100), CarImage.CAR),
+                new Option("Car", new Position(CARS_X, OPTIONS_Y + 30), new FontImageFactory().getImageRepresentation("CAR"))
+        );
+
+        when(customizeMenuMock.getMenuLevels()).thenReturn(Arrays.asList(Joe, Kids, Cars));
+
+
 
     }
 
@@ -100,6 +130,30 @@ public class CustomizeMenuViewerTest {
         customizeMenuViewer.drawInformation(guiMock);
 
         verify(guiMock).drawImage(new Position(4, 4), KEY_ESC);
+    }
+
+    @Test
+    public void testDrawElementsJoe() {
+        when(customizeMenuMock.isSelectedJoeCustomize()).thenReturn(true);
+        customizeMenuViewer.drawElements(guiMock);
+
+        verify(guiMock, times(28)).drawImage(any(Position.class), any(String[].class));
+    }
+
+    @Test
+    public void testDrawElementsKids() {
+        when(customizeMenuMock.isSelectedKidsCustomize()).thenReturn(true);
+        customizeMenuViewer.drawElements(guiMock);
+
+        verify(guiMock, times(28)).drawImage(any(Position.class), any(String[].class));
+    }
+
+    @Test
+    public void testDrawElementsCars() {
+        when(customizeMenuMock.isSelectedCarsCustomize()).thenReturn(true);
+        customizeMenuViewer.drawElements(guiMock);
+
+        verify(guiMock, times(28)).drawImage(any(Position.class), any(String[].class));
     }
 
 }
